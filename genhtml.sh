dde9a0a44c11f4bc5d1bf09471b18395d6629b5f
rodrigo-bruno <rbruno@gsd.inesc-id.pt>
2017-04-09 00:38:11 +0100

    Added collapsible sections.

--- genhtml.sh
+++ genhtml.sh
@@ -7,9 +7,19 @@ do
     do
       stats="$stats\n<p>$ln</p>"
     done < $graphdir/stats
+
+    stats="<div data-role="main" class="ui-content">
+	   <div data-role="collapsible">
+	     <h1>Stats</h1>$stats</div></div>"
+
     for png in $graphdir/*.png
     do
-        graphs="$graphs <h3 style=\"text-align:center;margin:1px 0px;letter-spacing:2px;\">$graphdir</h3>\n$stats\n<img src=\"$(realpath $png)\" style=\"max-width:100%;height:auto\">\n"
+	graphs="$graphs
+		<div data-role="main" class="ui-content">
+		<div data-role="collapsible">
+			<h1>$(basename $graphdir)</h1>
+			\n$stats\n <img src=\"$(realpath $png)\" style=\"max-width:100%;height:auto\">
+		</div></div>"
     done
 done
 echo -e "
@@ -17,9 +27,10 @@ echo -e "
 <html>\n
  <head>\n\
   <meta charset=\"utf-8\">\n\
-  <meta name=\"viewport\" content=\"width=device-width,
-                                    initial-scale=1\">\n\
-
+  <meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">\n\
+  <link rel=\"stylesheet\" href=\"https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css\">\n\
+  <script src=\"https://code.jquery.com/jquery-1.11.3.min.js\"></script>\n\
+  <script src=\"https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js\"></script>\n\
  </head>\n\
  <body>\n\
   <div>\n\
