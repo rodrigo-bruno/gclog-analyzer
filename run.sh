e8358bc872e3668bb002a648bb042d8d408cf6a7
Duarte Patrício <duarte.carvalho@tecnico.ulisboa.pt>
2017-04-09 11:56:26 +0100

    User must pass the gc log in the first arg on the run.sh script. This is to be able to use multiple named logs.

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 home=$(dirname $0)
-gc_log=cassandra-gc.log
+gc_log="$1"
+shift
 
 for input_dir in "$@"
 do
-	echo "Processing $input_dir..."
-	echo "Stats for $input_dir" > $input_dir/stats
-	$home/process.sh $input_dir/$gc_log >> $input_dir/stats
-	$home/plot.sh $input_dir
-	echo "Processing $input_dir... Done"
+    echo "Processing $input_dir..."
+    echo "Stats for $input_dir" > $input_dir/stats
+    $home/process.sh $input_dir/$gc_log >> $input_dir/stats
+    $home/plot.sh $input_dir
+    echo "Processing $input_dir... Done"
 done
-$home/genhtml.sh $@
+$home/genhtml.sh "$@"
+
+exit $?
